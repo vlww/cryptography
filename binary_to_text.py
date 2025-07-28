bd7e33fa49a5fc0d23b4f43b366a2ab5be7ed66c
vlww <victorcalehr18@gmail.com>
2025-07-28 10:46:57 -0400

    allow non-digit characters

--- binary_to_text.py
+++ binary_to_text.py
@@ -2,9 +2,15 @@ import math
 jit = input("Enter the encrypted message: ")
 numbers_to_lock_in = jit.split(" ")
 for number in numbers_to_lock_in:
-    decimal = 0
-    for i in range(len(number)):
-        gurt = math.pow(2, i)
-        decimal += gurt*int(number[len(number)-i-1])
-    print(chr(int(decimal) + 64), end="")
+    if not number.isnumeric():
+        if number == "|":
+            print(" ", end="")
+        else:
+            print(number, end="")
+    else:
+        decimal = 0
+        for i in range(len(number)):
+            gurt = math.pow(2, i)
+            decimal += gurt*int(number[len(number)-i-1])
+        print(chr(int(decimal) + 64), end="")
 print()
