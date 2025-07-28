f7e825b78e447b552fbc057a8d40ffbe66173acf
vlww <victorcalehr18@gmail.com>
2025-07-28 10:53:04 -0400

    rizz

---
+++ caesar_cipher_decoder.py
@@ -0,0 +1,15 @@
+text = input("Enter the encrypted message: ")
+shift = -3
+result = ""
+
+for char in text:
+    if char.isalpha():
+        base = ord('A') if char.isupper() else ord('a')
+        result += chr((ord(char) - base - shift) % 26 + base)
+    else:
+        result += char 
+
+print(result)
+
+
+
