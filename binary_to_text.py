32ff480ced6373e2d8baa031193803452b7dd779
vlww <victorcalehr18@gmail.com>
2025-07-28 10:12:50 -0400

    split by space

--- binary_to_text.py
+++ binary_to_text.py
@@ -1,7 +1,7 @@
 import math
 
 jit = input("Enter the encrypted message: ")
-numbers_to_lock_in = jit.split(", ")
+numbers_to_lock_in = jit.split(" ")
 
 for number in numbers_to_lock_in:
     decimal = 0
