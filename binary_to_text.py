30aa43c805b61e2ed3227ed0618aeca36fbece2d
vlww <victorcalehr18@gmail.com>
2025-07-28 10:10:41 -0400

    binary to text

--- binary_to_text.py
+++ binary_to_text.py
@@ -1,10 +1,21 @@
 import math
 
-number = input("Enter the encrypted message: ")
+jit = input("Enter the encrypted message: ")
+numbers_to_lock_in = jit.split(", ")
 
-decimal = 0
-for i in range(len(number)):
-    gurt = math.pow(2, i)
-    decimal += gurt*int(number[len(number)-i-1])
+for number in numbers_to_lock_in:
+    decimal = 0
+    for i in range(len(number)):
+        gurt = math.pow(2, i)
+        decimal += gurt*int(number[len(number)-i-1])
 
-print(int(decimal))
+    key = {
+        1: "A", 2: "B", 3: "C", 4: "D", 5: "E",
+        6: "F", 7: "G", 8: "H", 9: "I", 10: "J",
+        11: "K", 12: "L", 13: "M", 14: "N", 15: "O",
+        16: "P", 17: "Q", 18: "R", 19: "S", 20: "T",
+        21: "U", 22: "V", 23: "W", 24: "X", 25: "Y",
+        26: "Z"
+    }
+
+    print(key[int(decimal)], " ")
